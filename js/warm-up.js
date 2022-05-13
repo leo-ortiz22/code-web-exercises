7ed71fca276eb43fe691346851cad335f20dd031
leo ortiz <leo.ortiz0913@gmail.com>
2022-05-13 09:09:04 -0500

    dom preactice

--- js/warm-up.js
+++ js/warm-up.js
@@ -33,7 +33,7 @@
 
 // Create a function, createNumObject, that takes in two numbers and returns an object with the following properties:
 
-{
+// {
 //
 //     lowest: LOWEST_NUMBER_HERE,
 //         highest: HIGHEST_NUMBER_HERE
@@ -94,6 +94,63 @@
 //             }
 //         }
 //     }}}
+//
+//     let classes = [
+//     {class: "6th grade history", students: 18},
+//     {class: "7th grade history", students: 20},
+//     {class: "8th grade history", students: 22}
+// ];}
 
+// Write a function, calculateTotalStudents, that returns the total number of students recorded in the classes array.
 
+// function calculateTotalStudents(classes) {
+// return classes
+// }
+// Bonus: instead of the total students, return the highest number of students in any class
 
+// Write a function called convertToObject that takes in a string that is the name of a class, and a number that is the number of students, and returns an object with the properties `class` and `students`
+
+//convertToObject("Intro to Programming", 20) returns {class: "Intro to Programming", students: 20} 
+function convertToProject() {
+    
+}
+var neighborhood1 ={
+    neighborhood: "Lovely Estates",
+    medianHomePrice: 280000,
+    pool: true,
+    tennis: false,
+    crimeRate: "low",
+    schools: [
+        {name: "ES1", rating: 8},
+        {name: "MS2", rating: 6},
+        {name: "HS3", rating: 8}
+    ]
+}
+
+var neighborhood2 ={
+    neighborhood: "Luminous Estates",
+    medianHomePrice: 270000,
+    pool: true,
+    tennis: false,
+    crimeRate: "low",
+    schools: [
+        {name: "ES1", rating: 8},
+        {name: "MS2", rating: 8},
+        {name: "HS3", rating: 8}
+    ]
+}
+
+var neighborhood3 ={
+    neighborhood: "Ginormous Ego Estates",
+    medianHomePrice: 350000,
+    pool: true,
+    tennis: true,
+    crimeRate: "low",
+    schools: [
+        {name: "ES1", rating: 9},
+        {name: "MS2", rating: 9},
+        {name: "HS3", rating: 9}
+    ]
+}
+
+// Write a function that takes a neighborhood object and determines if it is desirable. A neighborhood is desirable if the median home price is less than 300000, crime rates are low, and the total rating of schools is at least 24.
